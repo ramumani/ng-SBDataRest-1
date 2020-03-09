41295e98996204e96e1dd6f0a55deab6efdd70ae
ramu.mani@hotmail.com <ramu.mani@hotmail.com>
2020-03-09 06:42:37 +0300

    Initial commit.

---
+++ onlinebookstore1/src/main/java/com/ramumani/onlinebookstore1/Onlinebookstore1Application.java
@@ -0,0 +1,13 @@
+package com.ramumani.onlinebookstore1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Onlinebookstore1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Onlinebookstore1Application.class, args);
+	}
+
+}
---
+++ onlinebookstore1/src/test/java/com/ramumani/onlinebookstore1/Onlinebookstore1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramumani.onlinebookstore1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Onlinebookstore1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
