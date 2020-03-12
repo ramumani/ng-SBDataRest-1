c227ec00027f369349b0d142ec31dac22dca55ea
ramu.mani@hotmail.com <ramu.mani@hotmail.com>
2020-03-12 06:38:48 +0300

    Angular front end - Initial commit.

---
+++ angular-bookstore/src/app/common/book.ts
@@ -0,0 +1,11 @@
+export class Book {
+    sku: string;
+    name: String;
+    description: String;
+    unitPrice: number;
+    imageUrl: String;
+    active: boolean;
+    unitsInStock: number;
+    createdOn: Date;
+    updatedOn: Date;
+}
